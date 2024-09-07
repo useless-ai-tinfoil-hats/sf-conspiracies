37a6dc9ec039bcbed8eeb3d7b9167916fda4b460
Walker Hughes <74113220+walkerhughes@users.noreply.github.com>
2024-09-07 13:45:00 -0700

    initial commit

---
+++ image_utils.py
@@ -0,0 +1,25 @@
+import os
+import dotenv
+
+dotenv.load_dotenv()
+
+from cloudflare import Cloudflare
+from IPython.display import display, Image
+import streamlit as st
+
+ACCOUNT_ID = st.secrets["ACCOUNT_ID"]
+CLOUDFLARE_API_TOKEN = st.secrets["CLOUDFLARE_API_TOKEN"]
+MODEL_NAME = st.secrets["MODEL_NAME"]
+
+client = Cloudflare(api_token=st.secrets["CLOUDFLARE_API_TOKEN"])
+
+def generate_and_display_image_from_summary(summary: str): 
+    """
+    Generate and display an image with @cf/bytedance/stable-diffusion-xl-lightning via Cloudflare Workers AI API.
+    """
+    data = client.workers.ai.with_raw_response.run(
+        model_name=MODEL_NAME,
+        account_id=ACCOUNT_ID,
+        prompt=summary
+    )
+    display(Image(data.read()))
