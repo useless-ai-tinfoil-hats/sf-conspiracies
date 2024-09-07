1ce70d17d74f5508a082a4d88801aba0342863eb
Tyler Gallup <galluptj@gmail.com>
2024-09-07 13:29:49 -0700

    Finished hardcoded mic pipeline

--- app.py
+++ app.py
@@ -47,16 +47,17 @@ def load_app():
     summary_text_embedder = SentenceTransformersTextEmbedder(model="sentence-transformers/all-MiniLM-L6-v2")
 
 
-    template = """
-    Using the context provided, generate a creative and outlandish conspiracy theory in a paragraph (Your total response (including title and references) MUST be 800 characters or less). 
-    Your theory should be based on the following news articles about events in San Francisco from the last year.
-    Make sure the theory is wild but still incorporates specific details news articles.
-    At the beginning of your answer, provide a catchy title for the theory in 7 or fewer words.
-    Bold this title using Markdown.
-    Don't use the word "Conspiracy" in your title.
-    In addition, center the title and have it on its own unique line.
-    Don't have a colon at the end of the title
-    At the end of your answer, provide the titles of the articles you used in a bulleted list.
+template = """
+generate a creative and outlandish conspiracy theory in two paragraphs
+Your theory should be based on the news articles about events in San Francisco from the last year, provided below on the Context.
+Make sure the theory is outlandish but still incorporates specific details from news articles that could only be known if you read them.
+Tell the theory as if it is the truth. Don't explicitly say you're pulling from news articles, just do it.
+At the beginning of your answer, provide a catchy title for the theory in 7 or fewer words.
+Bold this title using Markdown.
+Don't use the word "Conspiracy" in your title.
+In addition, center the title and have it on its own unique line.
+Don't have a colon at the end of the title
+At the end of your answer, provide the titles of the articles you used in a bulleted list.
 
     Context:
     Below are the key news articles that occurred in San Francisco over the last year. 
@@ -69,44 +70,30 @@ def load_app():
     Answer:
     """
 
-    mic_template = """
-    Based on the following conversation, generate a creative and outlandish conspiracy theory in a paragraph
-    (Your total response (including title and references) MUST be 800 characters or less). 
-    Your theory should be based on the following news articles about events in San Francisco from the last year.
-    Make sure the theory is wild but still incorporates specific details news articles.
-    At the beginning of your answer, provide a catchy title for the theory in 7 or fewer words.
-    Bold this title using Markdown.
-    Don't use the word "Conspiracy" in your title.
-    In addition, center the title and have it on its own unique line.
-    Don't have a colon at the end of the title
-    At the end of your answer, provide the titles of the articles you used in a bulleted list.
+text_template = "Using the prompt below in the Question provided, " + template
 
-    Context:
-    Below are the key news articles that occurred in San Francisco over the last year. 
-    Use them to inform your answer:
-    {% for document in documents %}
-        {{ document.content }}
-    {% endfor %}
+mic_template = """Connect the following conversation indicated in the Question to news 
+    articles in the Context provided (infer the speaker names based on the conversation 
+    and if you can't infer the names, just call them San Francisco Residents), and """ + template
 
-    Question: {{question}}
-    Answer:
-    """
 
-    summary_template = """
-    Given the following input with a conspiracy title, description, and sources, create a highly 
-    detailed and vivid scene that can be passed into an AI image generator based on the title and description.
-    The scene should include specific visual elements, actions, 
-    setting, and mood that reflect the core theme of the conspiracy theory. The prompt should be 
-    structured for optimal image generation.
+summary_template = """
+Given the following input with a conspiracy title, description, and sources, create a 
+detailed and vivid scene that can be passed into an AI image generator based on the title and description.
+The scene should include specific visual elements, actions, 
+setting, and mood that reflect the core theme of the conspiracy theory. The output should be 
+structured for optimal image generation.
+(Your total response MUST be 800 characters or less).
 
     **Conspiracy Theory Description**:
     {{input}}
 
-    **Requirements**:
-    1. **Setting**: Where is the scene taking place? Provide a vivid description of the location, time of day, weather, or other environmental factors.
-    2. **Main Characters/Objects**: In one sentence, describe what or who is in the scene? Include any important figures, objects, or entities (e.g., UFOs, secret agents, strange artifacts).
-    3. **Key Actions**: What is happening? Describe any movements, interactions, or behaviors.
-    4. **Atmosphere and Mood**: Set the tone of the image (e.g., eerie, mysterious, futuristic) in one sentence.
+**Requirements**:
+1. **Setting**: Where is the scene taking place? Provide a vivid description of the location, time of day, weather, or other environmental factors.
+2. **Main Characters/Objects**: In one sentence, describe what or who is in the scene? Include any important figures, objects, or entities (e.g., UFOs, secret agents, strange artifacts).
+3. **Key Actions**: What is happening? Describe any movements, interactions, or behaviors.
+4. **Atmosphere and Mood**: Set the tone of the image (e.g., eerie, mysterious, futuristic) in one sentence.
+5. **Visual Details**: In a maximum of one sentence, focus on visual features such as color palette, textures, and any fine details that make the image compelling and related to the conspiracy.
 
     **Example Output**:
     A futuristic night-time scene in San Francisco, with a large UFO hovering silently above the Golden Gate Bridge. The bridge is dimly lit by streetlights, casting long shadows on the road. Dark clouds swirl ominously in the sky, as the UFO emits a faint, eerie blue glow. Below, several shadowy figures dressed in black suits and sunglasses are standing near the base of the bridge, staring upwards. The city in the background is barely visible through a thick, fog-like mist. The atmosphere is tense, with cool tones dominating the scene. The lighting is dramatic, with sharp contrasts between light and dark areas.
@@ -115,28 +102,19 @@ def load_app():
     """
 
 
-    temp_mic_prompt = """
-    Speaker A: Hi, Rafa. How are you doing?
-    Speaker B: Hi, Vitoria. I'm doing good. And you?
-    Speaker A: I'm doing good. What have you been up to?
-    Speaker B: Not much. Just walking here and there. Did you hear what happened to me the other day?
-    Speaker A: No, what happened?
-    Speaker B: I was walking through Union Square and I saw a ginormous quantity of pigeons.
-    Speaker A: Oh my God. Yes. You know, I was at the ferry building and there were so many there too. So annoying.
-    Speaker B: You know what it is? I think it's the fact that they're building now. Pigeonshe they're not really birds. They're there to monitor us. So they're cyborg agents done by the government in order to monitor the people so that they behave just like the government wants.
-    Speaker A: Damn. Do you really think that would be happening?
-    Speaker B: I don't know, but I don't trust the government that much, to be honest.
-    Speaker A: Fair enough.
-    """
-    # prompt_builder = PromptBuilder(template=template)
-    # basic_rag_pipeline = Pipeline()
-    # basic_rag_pipeline.add_component("text_embedder", text_embedder)
-    # basic_rag_pipeline.add_component("retriever", retriever)
-    # basic_rag_pipeline.add_component("prompt_builder", prompt_builder)
-    # basic_rag_pipeline.add_component("llm", OpenAIGenerator(model="gpt-3.5-turbo"))
-    # basic_rag_pipeline.connect("text_embedder.embedding", "retriever.query_embedding")
-    # basic_rag_pipeline.connect("retriever", "prompt_builder.documents")
-    # basic_rag_pipeline.connect("prompt_builder", "llm")
+temp_mic_prompt = """
+Speaker A: Hi, Rafa. How are you doing?
+Speaker B: Hi, Vitoria. I'm doing good. And you?
+Speaker A: I'm doing good. What have you been up to?
+Speaker B: Not much. Just walking here and there. Did you hear what happened to me the other day?
+Speaker A: No, what happened?
+Speaker B: I was walking through Union Square and I saw a ginormous quantity of pigeons.
+Speaker A: Oh my God. Yes. You know, I was at the ferry building and there were so many there too. So annoying.
+Speaker B: You know what it is? I think it's the fact that they're building now. Pigeonshe they're not really birds. They're there to monitor us. So they're cyborg agents done by the government in order to monitor the people so that they behave just like the government wants.
+Speaker A: Damn. Do you really think that would be happening?
+Speaker B: I don't know, but I don't trust the government that much, to be honest.
+Speaker A: Fair enough.
+"""
 
     prompt = temp_mic_prompt
     # Function to create the prompt builder based on the selected template
@@ -173,16 +151,6 @@ def load_app():
     input_pipeline = create_pipeline(True)
     summary_rag_pipeline = create_summary_pipeline()
 
-
-    # mic_prompt_builder = PromptBuilder(template=template)
-    # mic_basic_rag_pipeline = Pipeline()
-    # mic_basic_rag_pipeline.add_component("text_embedder", text_embedder)
-    # mic_basic_rag_pipeline.add_component("retriever", retriever)
-    # mic_basic_rag_pipeline.add_component("prompt_builder", prompt_builder)
-    # mic_basic_rag_pipeline.add_component("llm", OpenAIGenerator(model="gpt-3.5-turbo"))
-    # mic_basic_rag_pipeline.connect("text_embedder.embedding", "retriever.query_embedding")
-    # basic_rag_pipeline.connect("retriever", "prompt_builder.documents")
-    # basic_rag_pipeline.connect("prompt_builder", "llm")
 
 
     st.title("👁️‍🗨️🌉 SF Conspiracy Theory Generator ")
